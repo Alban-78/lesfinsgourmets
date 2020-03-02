4d15fc9d9b218519b49d4980c30eee5c3887e251
unknown <albanhusar@gmail.com>
2020-03-02 12:17:23 +0100

    changeread

--- Public/JS/cookie.js
+++ Public/JS/cookie.js
@@ -1,5 +1,18 @@
-function masquerBandeauCookies(){
-    let divBandeauCookies = document.getElementById("cookies");
-    divBandeauCookies.style.display = "none";
+document.getElementById("cookieok").addEventListener("click", hideCookiebar);
+function hideCookiebar() {
+  // JE CACHE MA BARRE
+  document.getElementById("cookie-bar").style.display = 'none';
+  // SI OUI JE STOCK DANS COOKIE AGREE
+  sessionStorage.setItem("cookiedac", "true")
+};
+if(sessionStorage.getItem("cookiedac") == "true"){
+ 
+  document.getElementById("cookie-bar").style.display = 'none';
 }
+//SI L'UTILISATEUR REFUSE
+document.getElementById("nocookie").addEventListener("click", redirection);
+function redirection() {
 
+  document.location.href="https://www.google.com/"
+
+};
--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # Documentation
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/50b9331192d56bbd79f8/maintainability)](https://codeclimate.com/github/Alban-78/lesfinsgourmets/maintainability)
+
+Les Fins Gourmets
+
+===================
+
+https://forthebadge.com/images/badges/powered-by-electricity.svg
+
+Ce projet a pour but de présenter une épicerie fine en ligne ainsi que ses produits.
+
+## Démarrage
+
+Aller sur le lien suivant https://github.com/Alban-78/lesfinsgourmets puis cliquer sur le bouton "Clone or download" puis sur le fichier index.html.
+
+## Fabriqué avec
+
+* Visual studio code
+* HTML5/CSS3
+* JavaScript
+* Bootstrap
+
+## Auteurs
+
+L'auteur du projet
+
+* Alban Husar
+
+## License
+
+
+
+
+
+
+
+
+
